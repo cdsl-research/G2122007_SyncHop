cc03658b8885e9c40ce50d27165efa106ed3be82
Junichi Kawatake <65758209+C0118083@users.noreply.github.com>
2023-07-21 12:13:39 +0900

    Update relay.py

--- relay.py
+++ relay.py
@@ -1,17 +1,15 @@
 from esp import espnow
-import utime
 
-ack=""
 try:
   e = espnow.ESPNow()
   e.init()
   print("init")
 
-  peer1 = b'\xecb`\x83\xbc\x10'   # MAC address of peer's wifi interface
-  peer2 = b'\xfc\xf5\xc4=;,'
-  e.add_peer(peer1)
-  e.add_peer(peer2)
-  print("add_peer: ", peer1, peer2)
+  recv_from = b'\xecb`\x83\xbc\x10'   # MAC address of peer's wifi interface
+  send_to = b'\xfc\xf5\xc4=;,'
+  e.add_peer(recv_from)
+  e.add_peer(send_to)
+  print("add_peer: ", recv_from, send_to)
 except:
   print("init_error")
 
@@ -19,25 +17,11 @@ except:
 while True:
   print("Receiving...")
   host, msg = e.irecv()     # Available on ESP32 and ESP8266
-  start=utime.ticks_ms()
   if msg:             # msg == None if timeout in irecv()
     print(host, msg)
     ack=str(msg)
-    if host==peer1:
-      stop=utime.ticks_ms()
-      f=open("result.txt","a")
-      f.write(str(stop-start))
-      f.write("\n")
-      f.close()
-      e.send(peer2, ack, True)
-      print("end")
-    if host==peer2:
-      stop=utime.ticks_ms()
-      f=open("result.txt","a")
-      f.write(str(stop-start))
-      f.write("\n")
-      f.close()
-      e.send(peer1, ack, True)
+    if host==recv_from:
+      e.send(send_to, msg, True)
       print("end")
     if msg == b'end':
       print("end")
